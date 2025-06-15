32e43b2e16dcd4e91f804ae701d45cef0e6bb01d
Tristan Gebeaux <Tristan.Gebeaux@gmail.com>
2025-06-15 19:22:21 -0400

    added todo and check_resources funcitons. Added general commenting to all parts fo the main.py file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 ### Data ###
-
 recipes = {
     "small": {
         "ingredients": {
@@ -26,6 +25,14 @@ recipes = {
         "cost": 5.5,
     }
 }
+"""could be turned into its own class but would increase overhead\n
+[small] [cost]
+        [ingredients]   {bread,ham,cheese}
+[medium] [cost]
+        [ingredients]   {bread,ham,cheese}
+[large] [cost]
+        [ingredients]   {bread,ham,cheese}
+"""
 
 resources = {
     "bread": 12,  ## slice
@@ -48,6 +55,10 @@ class SandwichMachine:
         }
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
+        for item in ingredients.keys():
+            if(0 > self.machine_resources[item]-ingredients[item]):
+                return False
+        return True
 
     def process_coins(self):
         """Returns the total calculated from coins inserted.
@@ -59,7 +70,16 @@ class SandwichMachine:
 
     def make_sandwich(self, sandwich_size, order_ingredients):
         """Deduct the required ingredients from the resources.
-           Hint: no output"""
+        WHY ARE SNADWICH_SIZE AND ORDER_INGREDIENTS BOTH PASSED    WHHHHHYYYYY WORLD
+        I am should only need one not both. but which is better"""
+        """match sandwich_size:
+            case "small":
+            case "medium":
+            case "large":
+            case _:
+                print("this should never run")
+                pause = input(f"error in make_sandwich\nsandwich_size=={sandwich_size}\n")
+"""
 
 ### Make an instance of SandwichMachine class and write the rest of the codes ###
 mySandwichMachine = SandwichMachine(resources)
@@ -69,21 +89,26 @@ while(userWantsToExit == False):
     userInput = input("What would you like?(small/medium/ large/ off/ report:")#did a quick google search ot make sure i got the syntax right
     userRequest=0
     """
+    if the below is changed then the possilbe values for sadnwich_size in SandwichMachine.make_sandwich must also be changed
     0=default value\n
     1=small\n
     2=medium\n
     3=large\n
     """
+    #TODO loop through options in the recipes dictionary instead
     match userInput:
         case "small"|"Small"|"SMALL":
             print("you requested a small sandwich")
             userRequest = 1
+            sandwichSize= "small"
         case "medium"|"Medium"|"MEDIUM":
             print("you requested a medium sandwich")
             userRequest = 2
+            sandwichSize = "medium"
         case "large"|"Large"|"LARGE":
             print("you requested a large sandwich")
             userRequest = 3
+            sandwichSize = "large"
         case "off"|"Off"|"OFF":
             print("you entered off")
             break
@@ -94,5 +119,8 @@ while(userWantsToExit == False):
             print(f"\"{userInput}\" is a invalid input")
             continue
     #only if input is large medium or small should the rest of this iteration of the loop run
-
+    if not (mySandwichMachine.check_resources(recipes[sandwichSize]["ingredients"])):
+        print("not enough resources")
+        continue
+    #mySandwichMachine.check_resources()
     #end of loop
